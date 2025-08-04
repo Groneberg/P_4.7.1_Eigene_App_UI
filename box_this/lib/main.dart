12a33161f9b9fa550b8c433718dce7431cadab64
Stefan Groneberg <stefan.groneberg@gmx.de>
2025-08-04 17:32:10 +0200

    adding TitleAppBar(custom widget)

--- box_this/lib/main.dart
+++ box_this/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:box_this/src/common/widgets/title_app_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,8 +12,15 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: TiTleAppBar(
+          title: "Home",
+          setBackIcon: false,
+        ),
+        body : Center(
+          child: Text(
+            'Welcome to Box This!',
+            style: TextStyle(fontSize: 24, color: Colors.black),
+          ),
         ),
       ),
     );
---
+++ box_this/lib/src/common/widgets/title_app_bar.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+
+class TiTleAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final bool setBackIcon;
+  // Icom icon;
+
+  const TiTleAppBar({
+    super.key,
+    this.title = "Default Title",
+    // this.setBackIcon = false,
+    this.setBackIcon = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      flexibleSpace: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [const Color(0xFF3A4801), const Color(0xFF7D9200)],
+            stops: [0.0, 1.0],
+          ),
+          border: Border.all(
+            color: const Color(0xFF3A4801),
+            width: 1.0, // 1px
+          ),
+        ),
+      ),
+      elevation: 0,
+      toolbarHeight: 48.0,
+      // TODO anpassen des PFeiles
+      automaticallyImplyLeading: setBackIcon,
+      leading: setBackIcon
+          ? IconButton(
+              icon: const Icon(
+                Icons.arrow_back_ios_new,
+                color: Color(0xFFFAFAFA),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          : null,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            title,
+            textAlign: TextAlign.end,
+            style: const TextStyle(
+              // TODO Schriftart anpassen
+              fontFamily: 'Roboto',
+              fontSize: 32,
+              fontWeight: FontWeight.w700,
+              color: Color(0xFFFAFAFA),
+            ),
+          ),
+        ],
+      ),
+      centerTitle: false,
+      actions: <Widget>[
+        // Padding(
+        //   padding: const EdgeInsets.only(right: 24.0), // Padding wie in deinem Original-Code
+        //   child: SvgPicture.asset(
+        //     'assets/icons/home_icon.svg', // Passe den Pfad zu deinem SVG an
+        //     width: 32,
+        //     height: 32,
+        //     colorFilter: const ColorFilter.mode(Color(0xFFFAFAFA), BlendMode.srcIn), // SVG einfärben
+        //   ),
+        // ),
+        IconButton(
+          icon: const Icon(Icons.home, size: 32, color: Color(0xFFFAFAFA)),
+          onPressed: () {
+            print('Home Icon gedrückt!');
+          },
+        ),
+      ],
+    );
+  }
+
+  @override 
+  Size get preferredSize => const Size.fromHeight(48.0); // Höhe der AppBar anpassen
+}
